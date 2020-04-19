5203bce37400e7a1fe049b0376ec5862a9484d09
mgdt <62079607+mgdt@users.noreply.github.com>
2020-04-20 01:03:41 +0400

    123

--- main.cpp
+++ main.cpp
@@ -32,16 +32,30 @@ void FillMatrix(int** a, int n)
 
 	int k = 0;
 	int t = 0;
-	for (int j = 0; j < ((n /2) + 1); j++) {
-		if (j % 2 == 0) {
-			for (int i = 0; i < n; i++)
-				a[i][j] = k++;
-		}
-		else {
-			for (int i = n - 1; i >= 0; i--)
-				a[i][j] = k++;
-		}
+	if (n % 2 == 1) {
+        for (int j = 0; j < ((n /2) + 1); j++) {
+            if (j % 2 == 0) {
+                for (int i = 0; i < n; i++)
+                    a[i][j] = k++;
+            }
+            else {
+                for (int i = n - 1; i >= 0; i--)
+                    a[i][j] = k++;
+            }
+        }
+	} else {
+        for (int j = 0; j < n /2; j++) {
+            if (j % 2 == 0) {
+                for (int i = 0; i < n; i++)
+                    a[i][j] = k++;
+            }
+            else {
+                for (int i = n - 1; i >= 0; i--)
+                    a[i][j] = k++;
+            }
+        }
 	}
+
 	if (n % 2 == 1) {
         for (int i = 0; i < n; i++) {
             if (i % 2 == 1) {
